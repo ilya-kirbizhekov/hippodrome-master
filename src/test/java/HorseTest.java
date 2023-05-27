59dc2e1281ca5d50020ff39f4ce7bec9f7831e60
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-05-27 13:39:06 +1000

    Merge remote-tracking branch 'origin/master'

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HorseTest {
@@ -26,7 +28,13 @@ public class HorseTest {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, ()->new Horse(name,1,1));
         assertEquals("Name cannot be blank.",e.getMessage());
     }
-
-
+    @Test
+    public void getName() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("qwerty", 1, 1);
+        Field name = Horse.class.getDeclaredField("name");
+        name.setAccessible(true);
+        String nameValue = (String) name.get(horse);
+        assertEquals("qwerty", nameValue);
+    }
 
 }
